9c5e9e87ebd35ea4b39b6e563f8443d307167921
ShreeyaGupta0 <146205042+ShreeyaGupta0@users.noreply.github.com>
2023-09-29 10:30:17 +0530

    main

---
+++ binarytoDecimal.java
@@ -0,0 +1,21 @@
+package Assignment_1;
+import java.util.*;
+public class binarytoDecimal {
+    public static void main(String args[]) {
+        int N,c=0,dec=0,n;
+		Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a binary no ");
+		N=sc.nextInt();
+		n=N;
+		while(n>0){
+		c+=1;
+		n/=10;
+		}
+		for(int i=0;i<=c;i++){
+			dec+=((N%10)*Math.pow(2,i));
+			N/=10;}
+		System.out.print("decimal value of binary no is "+dec);
+    }
+}
+    
+
